ce5f84678935e5f7a241288ef97330e39995bf96
ukirsche <ukirschenmann@gmx.de>
2018-11-19 21:38:32 +0100

    Issue #6: Würfelgröße und zu kleiner Room

--- Scripts/SetToggleColors.cs
+++ Scripts/SetToggleColors.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SetToggleColors : MonoBehaviour {
-    
+public class SetToggleColors : MonoBehaviour
+{
+
     public Toggle mutToggle, klugToggle, intuitionToggle, charismaToggle, ffToggle, gwToggle, koToggle, kkToggle;
 
+    #region Event Handlers
+    public void mutToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_MUT);
+    }
 
-    
+    public void klugheitToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_KLUG);
+    }
+
+    public void intuitionToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_INTUITION);
+
+    }
+
+    public void charismaToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_CHARISMA);
+    }
+
+    public void fingerfertigkeitToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_FF);
+    }
+
+    public void gewandtheitToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_GW);
+
+    }
+
+    public void konstitutionToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_KK);
+
+    }
+
+    public void koerperkraftToggleButtonClicked(bool activated)
+    {
+        setPrefabName(activated, ToolboxDice.DSA_D20_PREFAB_NAME_KK);
+
+    }
+
+    #endregion
+
+    #region Helpers zum Setzen der Prefabsnamen
+    private void setPrefabName(bool activated, string prefabName)
+    {
+        ToolboxDice diceVar = ToolboxDice.Instance;
+
+        if (activated)
+        {
+            diceVar.dsaChosenPrefabs.Add(prefabName);
+            Debug.Log(prefabName + " aktiviert");
+        }
+        else
+        {
+            diceVar.dsaChosenPrefabs.Remove(prefabName); 
+            Debug.Log(prefabName + " deaktiviert");
+        }
+    }
+    #endregion
 }
